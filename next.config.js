f6827910afc0b3a37eccef37817dff25eada908d
Gamalliel Sharon <you@example.com>
2022-08-19 11:43:12 +0700

    reflushed

--- next.config.js
+++ next.config.js
@@ -7,27 +7,4 @@ const nextConfig = {
   },
 };
 
-// const images = {
-//   domains: ['s4.anilist.co'],
-// };
-
 module.exports = nextConfig;
-// module.exports = {
-//   images: {
-//     domains: ['https://s4.anilist.co/'],
-//   },
-// };
-// module.exports = {
-//   webpack: (config) => {
-//     config.module.rules.push({
-//       test: /\.(graphql|gql)$/,
-//       exclude: /node_modules/,
-//       loader: 'graphql-tag/loader',
-//     });
-//     return config;
-//   },
-//   webpackDevMiddleware: (config) => {
-//     return config;
-//   },
-//   nextConfig,
-// };
